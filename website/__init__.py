2d6887148e9285c56b2af42c200f1212e0aca683
lalitbawa <101063178+lalitbawa@users.noreply.github.com>
2023-01-30 05:08:41 +0000

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -5,27 +5,24 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'lalitbawa123'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c22090185:Lalit12345@csmysql.cs.cf.ac.uk:3306/c22090185_database'
-    db.init_app(app)
 
-    from .views import views
-    from .auth import auth
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'lalitbawa123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c22090185:Lalit12345@csmysql.cs.cf.ac.uk:3306/c22090185_database'
+db.init_app(app)
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+from .views import views
+from .auth import auth
 
-    from .models import User,Note
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+from .models import User,Note
 
-    @login_manager.user_loader
-    def user_load(id):
-        return User.query.get(int(id))
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    return app
-
+@login_manager.user_loader
+def user_load(id):
+    return User.query.get(int(id))
